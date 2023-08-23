5b381ae10745126c519a53eda69e1dd023cbcc82
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-08-23 17:52:36 -0300

    feat: auth route added

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import "dotenv/config";
+
 const app = express();
-app.use(express.json()); //middleware serializer
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-
+app.use(cors({ credentials: true }));
+app.use(bodyParser.json()); //middleware serializer
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
