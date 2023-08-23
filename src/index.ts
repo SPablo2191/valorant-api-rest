02a1a39702017479df8a92e8168e79bd1c8e164b
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-08-23 16:52:40 -0300

    feat: mysql driver added

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,7 @@ const app = express();
 app.use(express.json()); //middleware serializer
 const PORT = 3000;
 
-app.get("/ping", (_req, res) => {
-  console.log("ping");
-  res.send("Que onda?");
-});
+
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
