3ad29c8bc383f1e2e6037dd2ca88aa26fec7cf1b
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-08-24 08:53:48 -0300

    feat: user router added

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,15 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import "dotenv/config";
+import router from "./routes/userRoutes";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({ credentials: true }));
 app.use(bodyParser.json()); //middleware serializer
+app.use('/api/users', router);
+
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
