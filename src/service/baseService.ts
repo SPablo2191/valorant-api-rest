3d486965dda43de8b3b341ba96f428a02b7c5cb2
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-08-24 09:58:35 -0300

    fix: columns with type

--- src/service/baseService.ts
+++ src/service/baseService.ts
@@ -1,7 +1,7 @@
-import { MySqlTable } from "drizzle-orm/mysql-core";
+import { MySqlTable,SelectedFields } from "drizzle-orm/mysql-core";
 import { db } from "../database/databaseConfig";
 
-export async function findAll<Model>(table: MySqlTable,columns? : any): Promise<Model[]> {
-  const results = await db.select(columns).from(table);
+export async function findAll<Model>(table: MySqlTable,columns? : SelectedFields): Promise<Model[]> {
+  const results = columns? (await db.select(columns).from(table)) : (await db.select().from(table));
   return results as Model[];
 }
